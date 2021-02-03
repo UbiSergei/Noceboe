ccf4a0b81cb2c47c61a3789bcf9c2369fc2c31f5
Lasuiro Suaveau Cheaoby <ubisergei@gmail.com>
2021-02-03 08:16:01 -0800

    Update Noceboe.hlsl

--- Noceboe.hlsl
+++ Noceboe.hlsl
@@ -3,10 +3,11 @@ float2
 tex:TEXCOORD0):COLOR{ float4
 //float3 float5
 nb=tex2D(s0,((tex //float1 float4
-	%6-.0004)*1.0023/1.013+.005)
+	%1.195)/.998-.001)
 	)
 
 	
-	*.9803
+	/1.308
+	%2642
 
 	;return nb; };
